f6e28bcb5b737618a5957fceed02e174a5ffdf43
jucasoft <lluuccaa1@gmail.com>
2023-11-07 19:52:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(80, () => {
+    console.log(`app listening at http://localhost:${port}`);
+});
+
 module.exports = app;

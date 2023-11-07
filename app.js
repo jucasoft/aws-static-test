8610ab678a5cf26423b6f53105661e1fdd100108
jucasoft <lluuccaa1@gmail.com>
2023-11-07 20:09:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var port = 4000;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 });
 
